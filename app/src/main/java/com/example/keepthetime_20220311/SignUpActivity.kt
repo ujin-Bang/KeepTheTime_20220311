ac07cff47e32afc49ff26c3852a60fa309573019
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-11 16:56:50 +0900

    회원가입 클릭시 입력값 추출

--- app/src/main/java/com/example/keepthetime_20220311/SignUpActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/SignUpActivity.kt
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20220311.databinding.ActivitySignInBinding
+import com.example.keepthetime_20220311.databinding.ActivitySignUpBinding
 
 class SignUpActivity : BaseActivity() {
 
-    lateinit var binding: ActivitySignInBinding
+    lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +20,12 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        binding.btnSignup.setOnClickListener {
+      binding.btnSignup.setOnClickListener {
 
-            val myIntent = Intent(mContext, SignInActivity::class.java)
-            startActivity(myIntent)
-        }
+          val inputEamil = binding.edtEmail.text.toString()
+          val inputPw = binding.edtPssword.text.toString()
+          val inputNickname = binding.edtNickname.text.toString()
+      }
 
     }
 
