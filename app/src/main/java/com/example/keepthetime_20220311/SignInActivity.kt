11e6ff21fe7fbbc8f863c406d0a4087b86753620
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-11 16:38:34 +0900

    주석 추가(rounOnUIThread)

--- app/src/main/java/com/example/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/com/example/keepthetime_20220311/SignInActivity.kt
@@ -3,6 +3,7 @@ package com.example.keepthetime_20220311
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.keepthetime_20220311.api.APIList
 import com.example.keepthetime_20220311.api.ServerAPI
@@ -42,6 +43,12 @@ class SignInActivity : BaseActivity() {
                     if(response.isSuccessful){
 //                        모든 결과가 최종 성공인 경우(code = 200으로 내려온 경우)
 //                        response.body() 이용
+
+                       val br = response.body()!! // 성공시 무조건 본문이 있다.=>BasicResponse형태의 변수로 파싱되어 나옴.
+
+//                        Retrofit의 Callback은 UIThread 안으로 다시 돌아오도록 처리되어있다.
+//                        UI 조작을 위해 rundOnUiThread{}작성 필요 없음
+                        Toast.makeText(mContext, br.message, Toast.LENGTH_SHORT).show()
                     }
 //                    실패인지?
                     else {
