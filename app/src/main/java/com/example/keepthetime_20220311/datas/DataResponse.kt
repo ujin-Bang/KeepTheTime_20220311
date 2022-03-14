e9f4d56088a8d5edf9143595205495822d58cba3
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-14 10:08:33 +0900

    응답 내용 파싱 > UI 반영  - 로그인에서 파싱했던 구조와 동일하게 파싱됨  - 미리 파싱해둔 구조를 재활용

--- app/src/main/java/com/example/keepthetime_20220311/datas/DataResponse.kt
+++ app/src/main/java/com/example/keepthetime_20220311/datas/DataResponse.kt
@@ -2,6 +2,11 @@ package com.example.keepthetime_20220311.datas
 
 // 서버응답 중, data:{}를 파싱하기 위한 클래스
 
+// 여러 API에서 data: {}를 내려줄 예정 => 내부 구성 모양이 각각 다를 수 있다.
+// ex. user:{}만 있다던지
+
+// 모든 경우의 대해 전부 변수를 생성해두고 => 활용시에만 주의 하면 됨
+
 class DataResponse(
     val user: UserData,
     val token: String,
