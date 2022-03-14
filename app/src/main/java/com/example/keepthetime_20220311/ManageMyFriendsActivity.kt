15a6a1b23240e8d2823362f4571ca4a9451c4c61
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-14 12:17:11 +0900

    내 친구 목록 관리하는 화면 추가

---
+++ app/src/main/java/com/example/keepthetime_20220311/ManageMyFriendsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.keepthetime_20220311
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.keepthetime_20220311.databinding.ActivityManageMyFriendsBinding
+
+class ManageMyFriendsActivity : BaseActivity() {
+
+    lateinit var binding: ActivityManageMyFriendsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_manage_my_friends)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
