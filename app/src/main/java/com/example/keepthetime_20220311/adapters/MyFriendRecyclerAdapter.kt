c764ec35ef1929144aa7339fe61f3f0a85836600
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-15 10:56:42 +0900

    내 친구 목록 - 리싸이클러뷰로 표시

--- app/src/main/java/com/example/keepthetime_20220311/adapters/MyFriendRecyclerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_20220311/adapters/MyFriendRecyclerAdapter.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.keepthetime_20220311.R
 import com.example.keepthetime_20220311.datas.UserData
 
@@ -14,6 +17,40 @@ class MyFriendRecyclerAdapter(
 ): RecyclerView.Adapter<MyFriendRecyclerAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+        val imgProfile = view.findViewById<ImageView>(R.id.imgProfile)
+        val txtNickname = view.findViewById<TextView>(R.id.txtNickname)
+        val imgSosialLogo = view.findViewById<ImageView>(R.id.imgSosialLogo)
+        val txtEmail = view.findViewById<TextView>(R.id.txtEmail)
+
+        fun bind(data: UserData){
+
+                Glide.with(mContext).load(data.profile_img).into(imgProfile)
+                txtNickname.text = data.nick_name
+            when (data.provider) {
+                "default" -> {
+                    imgSosialLogo.visibility = View.GONE
+
+                    txtEmail.text = data.email
+                }
+                "kakao" -> {
+                    imgSosialLogo.visibility = View.VISIBLE
+                    imgSosialLogo.setImageResource(R.drawable.kakao_logo)
+                    txtEmail.text = "카카오 로그인"
+                }
+                "facebook" -> {
+                    imgSosialLogo.visibility = View.VISIBLE
+                    imgSosialLogo.setImageResource(R.drawable.facebook_logo)
+                    txtEmail.text = "페이스북 로그인"
+                }
+                "naver" -> {
+                    imgSosialLogo.visibility = View.VISIBLE
+                    imgSosialLogo.setImageResource(R.drawable.naver_logo)
+                    txtEmail.text = "네이버 로그인"
+                }
+            }
+
+        }
 
     }
 
@@ -25,6 +62,8 @@ class MyFriendRecyclerAdapter(
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
+        val data = mList[position]
+        holder.bind(data)
     }
 
     override fun getItemCount()= mList.size
